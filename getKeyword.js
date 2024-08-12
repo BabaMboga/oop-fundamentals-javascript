e9410acceb99c1e53ed145892a6f7e3725ffeeb6
Ayim William <odhisayim@gmail.com>
2024-08-12 07:30:49 +0300

    added the getKeyword file

---
+++ getKeyword.js
@@ -0,0 +1,21 @@
+class Square {
+    constructor(sideLength) {
+      this.sideLength = sideLength;
+    }
+  
+    get area() {
+      return this.sideLength * this.sideLength;
+    }
+  }
+
+
+const square = new Square(5);
+console.log(square.sideLength);
+console.log(square.area);
+
+// class Square {
+//     constructor(sideLength) {
+//       this.sideLength = sideLength;
+//       this.area = sideLength * sideLength;
+//     }
+//   }
